e4971338bd52f2ab1e03b7027680510b58a38d16
afik.cohen <aphexsa@gmail.com>
2023-08-19 23:55:44 -0700

    Added functionality to load a specific chat model, retrieve messages from an endpoint, generate a response based on the messages, and post the response as a thought.

--- sample_mlc_chat.py
+++ sample_mlc_chat.py
@@ -1,26 +1,68 @@
+import requests
+import time
+import psutil
 from mlc_chat import ChatModule
 from mlc_chat.callback import StreamToStdout
 
-# From the mlc-llm directory, run
-# $ python sample_mlc_chat.py
+CHATMODEL = "Llama-2-70b-chat-hf-q4f16_1"
 
+print(f"Loading {CHATMODEL}...")
 # Create a ChatModule instance
-cm = ChatModule(model="Llama-2-70b-chat-hf-q4f16_1")
-# You can change to other models that you downloaded, for example,
-# cm = ChatModule(model="Llama-2-13b-chat-hf-q4f16_1")  # Llama2 13b model
+cm = ChatModule(model=CHATMODEL)
+print(f"{CHATMODEL} loaded")
+print(f"Current RAM usage: {psutil.Process().memory_info().rss / 1024 ** 2} MB")
 
-output = cm.generate(
-    prompt="What is the meaning of life?",
-    progress_callback=StreamToStdout(callback_interval=2),
-)
+# Define the GET and POST endpoints
+get_endpoint = "http://10.53.1.86:8080/messages"
+post_endpoint = "http://10.53.1.86:8080/newUserMessage"
 
-# Print prefill and decode performance statistics
-print(f"Statistics: {cm.stats()}\n")
+# Define the maximum number of retries for failed operations
+max_retries = 3
 
-output = cm.generate(
-    prompt="How many points did you list out?",
-    progress_callback=StreamToStdout(callback_interval=2),
-)
+def get_messages():
+    print("Getting messages...")
+    for _ in range(max_retries):
+        try:
+            response = requests.get(get_endpoint)
+            messages = response.json()
+            print(f"Got {len(messages)} messages")
+            return messages
+        except Exception as e:
+            print(f"Error getting messages: {e}")
+    return None
 
-# Reset the chat module by
-# cm.reset_chat()
+def generate_response(messages):
+    print("Generating response...")
+    with open('prompt.txt', 'r') as file:
+        prompt = file.read()
+    prompt += "\n".join([msg["str"] for msg in messages])
+    for _ in range(max_retries):
+        try:
+            response = cm.generate(
+                prompt=prompt,
+                progress_callback=StreamToStdout(callback_interval=2),
+            )
+            print("Response generated")
+            return response
+        except Exception as e:
+            print(f"Error generating response: {e}")
+    return None
+
+def post_message(output):
+    print("Posting new thought...")
+    for _ in range(max_retries):
+        try:
+            requests.post(post_endpoint, json={"message": output})
+            print("Thought posted")
+            return True
+        except Exception as e:
+            print(f"Error posting message: {e}")
+    return False
+
+while True:
+    messages = get_messages()
+    if messages is not None:
+        output = generate_response(messages)
+        if output is not None:
+            post_message(output)
+    time.sleep(60)
